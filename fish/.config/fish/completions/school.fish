265b4aef362cee46617a0d2edf995ffa9d150d69
GamerGbesh <gbesh.12@gmail.com>
2026-04-19 18:19:53 +0000

    Added a fish function to yazi into my courses using 'school'

---
+++ fish/.config/fish/completions/school.fish
@@ -0,0 +1,2 @@
+complete -c school -e
+complete -c school -f -a "(find /mnt/windows/Users/Philemon/Desktop/L300/ -maxdepth 1 -printf \"%f\n\")"
---
+++ fish/.config/fish/functions/school.fish
@@ -0,0 +1,3 @@
+function school --wraps='cd /mnt/windows/Users/Philemon/Desktop/L300 && yazi' --description 'alias school=cd /mnt/windows/Users/Philemon/Desktop/L300 && yazi'
+    cd /mnt/windows/Users/Philemon/Desktop/L300 && yazi $argv
+end
