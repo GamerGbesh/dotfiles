25966723e90babdc179358b2a5710547831db894
Philemon <gbesh.12@gmail.com>
2026-04-23 23:57:05 +0000

    Worked on variables

--- fish/.config/fish/completions/hyprconf.fish
+++ fish/.config/fish/completions/hyprconf.fish
@@ -1,2 +1,2 @@
 complete -c hyprconf -e
-complete -c hyprconf -f -a "(find ~/.config/hypr -maxdepth 1 -printf \"%f\n\")"
+complete -c hyprconf -f -a "(string split ' ' (basename -a ~/.config/hypr/*))"
