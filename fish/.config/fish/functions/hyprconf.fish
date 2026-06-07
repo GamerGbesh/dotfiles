2c8dc3ad38301a2c1b749745b13e159f96abbb29
Philemon <gbesh.12@gmail.com>
2026-06-07 15:11:57 +0000

    Changed hyprconf to open nvim in the folder if no args are provided

--- fish/.config/fish/functions/hyprconf.fish
+++ fish/.config/fish/functions/hyprconf.fish
@@ -1,6 +1,12 @@
 function hyprconf --wraps='cd ~/.config/hypr; and nvim; and cd -' --wraps='cd ~/.config/hypr && nvim $argv; and cd -'
     set -l prev $PWD
     builtin cd ~/.config/hypr
-    nvim $argv
+
+    set -l target .
+    if test (count $argv) -gt 0
+        set target $argv
+    end
+    nvim $target
+
     builtin cd $prev
 end
